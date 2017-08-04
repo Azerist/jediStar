ac559047db7450803c6d1ba7868362f46f591409
jbellionjourdan <jeremie.bellionjourdan@gmail.com>
2017-08-04 10:33:09 +0200

    Ajout de logs

--- src/main/java/fr/jedistar/Main.java
+++ src/main/java/fr/jedistar/Main.java
@@ -5,8 +5,12 @@ public class Main {
 	public static void main(String ... args) {
 
 		if(args.length != 0) {
+			System.out.println("Lancement du bot avec le token -"+args[0]+"-");
 			JediStarBot bot = new JediStarBot(args[0]);
 			bot.connect();
+		}
+		else {
+			System.out.println("Aucun token passé en paramètre");
 		}
 
 	}
